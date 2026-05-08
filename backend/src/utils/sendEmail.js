21517726b55930c4699a8174aba1b592473564f7
Devang Singh <devangsingh3007@gmail.com>
2026-05-09 03:01:48 +0530

    fix smtp IPv6 issue - force IPv4

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -8,6 +8,7 @@ const createTransporter = () => nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    family: 4, // use IPv4
 });
 
 export const sendVerificationEmail = async (to, token) => {
